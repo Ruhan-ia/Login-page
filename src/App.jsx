bd95dfa174ff1f13e028ae17f573ae574e42f984
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-10-29 08:20:41 +0600

    login page desiging start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Title from "./Component/Style/Title.styled"
+import styled from 'styled-components';
+
+
+function App() {
+
+  return (
+    <>
+    <Title><h3>hello there</h3></Title>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Style/Title.styled.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.div`
+color:red;
+`
+export default Title;

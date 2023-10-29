35aef3d667f165812233fc53c1cfb7409bf651c7
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-10-29 15:55:50 +0600

    login form complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,100 @@
-import Title from "./Component/Style/Title.styled"
 import styled from 'styled-components';
+import Container, {  Button, Card, Div, Div2, Field, ForgotPassword, Form, FormControl, Image, Input, Label, Submit, Terms, Title, Toggle } from './Component/Style/Global.styled';
+import pic from './assets/pic-1.svg'
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { useState } from 'react';
 
 
 function App() {
 
+  const [passwordVisible, setPasswordVisible] = useState(false);
+const [password, setPassword] = useState('');
+
+const togglePassword = () => {
+  setPasswordVisible(!passwordVisible);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault(); 
+};
   return (
     <>
-    <Title><h3>hello there</h3></Title>
-    </>
+  
+   <Container>
+    <Image>
+      <img src={pic} alt="login-svg" />
+    </Image>
+    <Field>
+      <Card>
+      <Title>
+      
+      Login
+    
+    </Title>
+      
+      <Form onClick={handleSubmit}>
+        <FormControl>
+          <Label>Login ID</Label>
+          <Input type='text' $w='100%' placeholder='enter login id'/>
+          
+        </FormControl>
+        <FormControl>
+        <Label>Password</Label>
+          <Div2>
+             <Input
+            type={passwordVisible ? 'text' : 'password'}
+            placeholder="password"
+            value={password}
+           $w='100%'
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            maxLength={6}
+          />
+          <Toggle onClick={togglePassword}>
+            {passwordVisible ? <BsEyeSlash/>:<BsEye/> }
+          </Toggle>
+          </Div2>
+
+        </FormControl>
+        <FormControl>
+        <Div>
+        <Label> 
+           <input type="checkbox">
+          </input>
+          Remember Me</Label>
+          <ForgotPassword>Change Password</ForgotPassword>
+        </Div>
+        <Label>  <input type="checkbox">
+          </input>
+          Agree to <Terms>Terms & Conditions</Terms>     </Label>
+        </FormControl>
+        <FormControl>
+          <Button>
+            <Submit>Login</Submit>
+          </Button>
+          
+        </FormControl>
+
+  <FormControl>
+  
+  <Button>
+       <Label>  
+          Don’t have an account? 
+          <Terms>Register Here</Terms>     </Label>
+  </Button>
+     
+        
+  </FormControl>
+      </Form>
+
+      </Card>
+    </Field>
+   
+   </Container>
+  
+  
+  </>
+    
   )
 }
 
---
+++ src/Component/Style/Global.styled.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export  const Title =styled.h1`
+color:  #04072F;
+text-align: center;
+font-family: Poppins;
+font-size: 48px;
+font-weight: 700;
+line-height: 50%; 
+
+
+
+  @media  (min-width: 1724px) {
+
+    font-size: 68px 
+   
+}
+
+
+`;
+export const Image =styled.section`
+width:40%;
+height:100%;
+padding-left:40px;
+@media (max-width: 768px) {
+    width:100%;
+    padding-left:0px;
+    height:auto
+  }
+
+  @media (min-width: 769px) and (max-width: 1023px) {
+   width:auto;
+   height:auto
+   padding-left:0px;
+  }
+`;
+export const Field =styled.section`
+width:40%;
+height:100%;
+margin:auto;
+justify-content: center;
+align-items: center;
+
+@media (max-width: 768px) {
+   width:100%;
+   height:auto;
+
+  }
+
+  @media (min-width: 769px) and (max-width: 1023px) {
+    width:auto;
+    height:auto
+  }
+`;
+export const Card = styled.div`
+width:80%;
+align-items: center;
+border: 2px ;
+margin:auto;
+border-radius: 13px;
+display:block;
+padding:80px;
+border-radius: 12px;
+background: #FFF;
+box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.16);
+
+@media (max-width: 768px) {
+    width:35%;
+
+  }
+
+  @media (min-width: 769px) and (max-width: 1023px) {
+    width:50%;
+  }
+`;
+
+// export const InputField =styled.input`
+// padding: 10px;
+//   font-size: 16px;
+//   border: 1px solid #ccc;
+//   border-radius: 4px;
+//   width: 100%;
+//   margin: 5px 0;
+//   &:focus {
+//     outline: none;
+//     border-color: #007bff;
+//   }
+// `;
+export const Form = styled.form`
+ 
+  margin:auto;
+`;
+
+export const FormControl = styled.div`
+  margin-bottom: 1rem;
+`;
+
+export const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  color:#737B86);
+font-size: 10px;
+font-weight: 300;
+line-height: 20px
+`;
+
+export const Input = styled.input`
+min-width: ${props=> props.$w};
+height:30px;
+border-radius: 8px;
+border: 1px solid rgba(4, 7, 47, 0.40);
+padding-left:6px;
+
+ 
+`;
+
+
+export const ForgotPassword = styled.a`
+  display: block;
+  color: #F78719;
+  text-decoration: none;
+  font-size: 10px;
+`;
+export const Terms = styled.a`
+  color: #F78719;
+  text-decoration: underline;
+`;
+ 
+export const Div= styled.div`
+display:flex;
+justify-content: space-between;
+`;
+export const Div2= styled.div`
+display:flex;
+justify-content: space-between;
+`;
+
+export const Button = styled.div`
+text-align:center;
+`;
+
+export const Submit = styled.button`
+  width:300px;
+
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  background:  #1575A7;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+
+
+  @media (max-width: 768px) {
+    width:200px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1023px) {
+    width:200px;
+  }
+`;
+
+
+
+export const Toggle = styled.button`
+  background: transparent;
+  border: none;
+  font-size: 15px;
+  cursor: pointer;
+  margin-left: -30px;
+`;
+
+
+
+ const Container =styled.section`
+display:flex;
+min-width:95%;
+min-height: 80vh;
+padding:15px;
+align-items: center;
+
+
+@media (max-width: 768px) {
+    display:block
+     
+  }
+
+  @media (min-width: 769px) and (max-width: 1023px) {
+  
+    display:block  
+   
+}
+
+`;
+export default Container;
